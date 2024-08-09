f2b25d30918dd35a51c3268c335b522ac0a3097a
rh-imesquit <ianbmesquita.dev@gmail.com>
2024-08-09 15:57:55 -0300

    feat: Creating the Javascript ws connection repo.

--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -4,9 +4,8 @@ import { sendMessage } from '../services/websocketService.js';
 const router = express.Router();
 
 router.post('/send', (req, res) => {
-    console.log("Body:", req.body)
-    console.log("Recebi uma mensagem via HTTP:", req.body.message);
-    sendMessage(req.body.message);
+    console.log("Recebi uma mensagem via HTTP:", req.body);
+    sendMessage(req.body);
     res.status(200).send("Mensagem enviada para o serviço externo via WebSocket.");
 });
 
